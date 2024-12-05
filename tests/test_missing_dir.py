64afde4237d30e74f94465241c412a89bbe9c1fa
greedydurian <aaronoh2015@gmail.com>
2024-12-05 16:28:19 +0800

    changes in tests

--- tests/test_missing_dir.py
+++ tests/test_missing_dir.py
@@ -1,13 +1,11 @@
 import unittest
-
 from treeline.core import generate_tree
 
-
-def test_non_existent_directory(self):
-    """Test handling of non-existent directory"""
-    with self.assertRaises(FileNotFoundError):
-        generate_tree("/path/that/does/not/exist")
-
+class TestMissingDirectory(unittest.TestCase):
+    def test_non_existent_directory(self):
+        """Test handling of non-existent directory"""
+        with self.assertRaises(FileNotFoundError):
+            generate_tree("/path/that/does/not/exist")
 
 if __name__ == "__main__":
     unittest.main()
