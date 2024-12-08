e4c716b3c2d96398e9a1a31a322d53049955d788
greedydurian <aaronoh2015@gmail.com>
2024-12-08 15:12:03 +0800

    fix: remove ambiguous l

--- treeline/__init__.py
+++ treeline/__init__.py
@@ -1,10 +1,17 @@
 import sys
 
 from .core import generate_tree
+from .diff_visualizer import DiffVisualizer
 
 
 def __call__(*args, **kwargs):
     return generate_tree(*args, **kwargs)
 
 
-sys.modules[__name__] = type("TreeLine", (), {"__call__": staticmethod(__call__)})()
+sys.modules[__name__] = type(
+    "TreeLine",
+    (),
+    {"__call__": staticmethod(__call__), "DiffVisualizer": DiffVisualizer},
+)()
+
+__all__ = ["generate_tree", "DiffVisualizer"]
