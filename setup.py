b8a6e120c57ea2f219830733807f089a0416fb29
greedydurian <aaronoh2015@gmail.com>
2024-12-11 12:58:30 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="treeline",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     package_data={
         "treeline": ["default_ignore"],
