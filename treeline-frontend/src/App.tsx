1128682f12fedb9adbdbb0be74d3ed45554f75d8
duriantaco <aaronoh2015@gmail.com>
2025-03-09 23:45:54 +0800

    fixes to impact and dependencies

--- treeline-frontend/src/App.tsx
+++ treeline-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VisualizationPage from './page/VisualizationPage';
 import NodeDetailsPage from './page/NodeDetailsPage';
+import ProjectMetricsPage from './page/ProjectMetricsPage';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<VisualizationPage />} />
         <Route path="/node/:nodeId" element={<NodeDetailsPage />} />
+        <Route path="/project-metrics" element={<ProjectMetricsPage />} />
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </Router>
